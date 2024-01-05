572468f3a8ccfff2ea575fe243bbb442da206913
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-05 15:36:35 +0530

    Understood the request methods in Epxress.js and also using those requests in the Postman

--- Week-2/index.js
+++ Week-2/index.js
@@ -2,9 +2,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Peter Parker");
+function totalSum(counter) {
+  var sum = 0;
+  for (var i = 1; i <= counter; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+app.get("/sum", (req, res) => {
+  var counter = req.query.counter7;
+  var calculatedSum = totalSum(counter);
+
+  res.send("The sum is " + calculatedSum);
 });
+
+// Post Handler
+function createUser(req, res) {
+  res.send("Post Handler");
+}
+app.post("/createUser", createUser);
+
+// Put Handler
+function putUser(req, res) {
+  res.send("Put Handler");
+}
+app.put("/putUser", putUser);
+
+// Delete Handler
+function deleteUser(req, res) {
+  res.send("Delete Handler");
+}
+app.put("/deleteUser", deleteUser);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
