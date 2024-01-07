992a02a42d1fdf0543c9eb9f6b1c7e7eb864cbbc
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-07 16:51:55 +0530

    Learned using Headers and Middlewares in Express.js

--- Week-1/main.js
+++ Week-1/main.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5173;
 
+function middleWare1(req, res, next) {
+  console.log("Going through middleware " + req.headers.number);
+  // res.send("Error from Inside Middleware");
+  next();
+}
+
+// Registering a middleware
+app.use(middleWare1);
+
 function doubleNum(num) {
   return num * 2;
 }
 
 app.post("/double", (req, res) => {
-  var num = req.query.number;
+  var num = req.headers.number;
   var ans = doubleNum(num);
   res.send("Post Handler " + ans);
 });
